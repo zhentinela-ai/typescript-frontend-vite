96a49d610c0d646635840231b5d4823953e95fb4
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-06-29 23:04:35 -0500

    crear tareas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const taskForm = document.querySelector<HTMLFormElement>("#taskForm");
+
+taskForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  interface Task {
+    title: string;
+    description: string;
+  }
+
+  let tasks: Task[] = [];
+
+  const title = taskForm["title"] as unknown as HTMLInputElement;
+  const description = taskForm["description"] as unknown as HTMLTextAreaElement;
+
+  tasks.push({
+    title: title.value,
+    description: description.value,
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+});
