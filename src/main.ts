b47e1aa16d9a08b00932b7069aa4f35e74c60eeb
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-06-29 23:58:42 -0500

    eliminar tareas

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import "toastify-js/src/toastify.css";
 import "./style.css";
+
+import { v4 as uuid } from "uuid";
+
+import Toastify from "toastify-js";
 
 const taskForm = document.querySelector<HTMLFormElement>("#taskForm");
 const tasksList = document.querySelector<HTMLDivElement>("#tasksList");
 
 interface Task {
+  id: string;
   title: string;
   description: string;
 }
@@ -19,14 +25,22 @@ taskForm?.addEventListener("submit", (e) => {
   tasks.push({
     title: title.value,
     description: description.value,
+    id: uuid(),
   });
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
 
-  renderTask(tasks)
+  Toastify({
+    text: "Task added",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d",
+    },
+  }).showToast();
 
-  taskForm.reset()
-  title.focus()
+  renderTask(tasks);
+
+  taskForm.reset();
+  title.focus();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -35,25 +49,31 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function renderTask(tasks: Task[]) {
+  tasksList!.innerHTML = "";
 
-  tasksList!.innerHTML = ""
-  
   tasks.forEach((task) => {
     const taskElement = document.createElement("div");
     taskElement.className =
       "bg-zinc-800 mb-1 p-4 rounded-lg hover:bg-zinc-700 hover:cursor-pointer";
 
     const header = document.createElement("header");
-    header.className = "flex justify-between"
+    header.className = "flex justify-between";
 
     const title = document.createElement("span");
     title.innerText = task.title;
 
-    header.append(title);
-    
     const btnDelete = document.createElement("button");
-    btnDelete.className = "bg-red-500 px-2 py-1 rounded-md"
-    btnDelete.innerText = "Delete"
+    btnDelete.className = "bg-red-500 px-2 py-1 rounded-md";
+    btnDelete.innerText = "Delete";
+
+    btnDelete.addEventListener("click", (e) => {
+      const index = tasks.findIndex((t) => t.id === task.id);
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTask(tasks);
+    });
+
+    header.append(title);
     header.append(btnDelete);
 
     const description = document.createElement("p");
@@ -61,6 +81,11 @@ function renderTask(tasks: Task[]) {
 
     taskElement.append(header);
     taskElement.append(description);
+
+    const id = document.createElement("p");
+    id.innerText = task.id;
+    id.className = "text-gray-400 text-xs";
+    taskElement.append(id);
 
     tasksList?.append(taskElement);
   });
